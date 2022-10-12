05ccd4d99361db07178a1f24a0668916bdcfebc5
erazchowdhury <erazchow@gmail.com>
2022-10-12 00:00:33 -0400

    prime number

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -12,6 +12,30 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
 
+        printPrimeNumbers(2, 1_000_000);
+
     }
+
+    public static void printPrimeNumbers (int start, int end) {
+
+        int counter;
+
+        for (int i = start; i <= end; i++) {
+            counter = 0;
+
+            for (int j = start + 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    counter++;
+                    break;
+                }
+            }
+
+            if (counter == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
 
 }
--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -8,9 +8,18 @@ public class UnitTestingMath {
 
     public static void main(String[] args) {
 
+        int[] array1 = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+        int[] array2 = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
+                67, 5, 679, 54, 32, 65};
+
         System.out.println(Factorial.getFactorialRecursion(5));
         System.out.println(Factorial.getFactorialIteration(6));
         System.out.println(Fibonacci.getFibonacci(10));
+        System.out.println(FindMissingNumber.findMissingNumber(array1));
+        System.out.println(LowestNumber.findLowestNumber(array2));
+
+        PrimeNumber.printPrimeNumbers(2, 20);
 
     }
 }
