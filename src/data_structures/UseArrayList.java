817a844b92d315c3204c3128b3543e1e7a05097a
erazchowdhury <erazchow@gmail.com>
2022-10-13 17:57:17 -0400

    finished UseArrayList.class

--- src/data_structures/UseArrayList.java
+++ src/data_structures/UseArrayList.java
@@ -1,6 +1,9 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class UseArrayList {
@@ -13,9 +16,9 @@ public class UseArrayList {
      * Store and retrieve the data from/to a database table.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
-        List<Integer> data =  new ArrayList<>();
+        List<Object> data =  new ArrayList<>();
 
         data.add(1);
         data.add(2);
@@ -31,6 +34,29 @@ public class UseArrayList {
         System.out.println(data);
 
         System.out.println(data.get(0));
+
+        System.out.println();
+
+        for (Object i: data) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        Iterator<Object> iterator = data.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        SharedStepsDatabase ssdb = new SharedStepsDatabase();
+
+        ssdb.insertList("array_list", "array_list_values", data);
+
+                String query = "SELECT * FROM ARRAY_LIST";
+        List<String> retrievedArrayList = ssdb.executeQueryReadAllSingleColumn(query, "array_list_values");
+
+        System.out.println(retrievedArrayList);
 
     }
 
